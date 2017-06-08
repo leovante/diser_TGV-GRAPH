9b3d818fcc729267a7ecbb0be101a2022485e49a
pro22 <leovante@live.ru>
2017-06-08 13:45:57 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+/**
+ * Created by pro22 on 08.06.2017.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Parsing pars = new Parsing();
+        pars.mainOne();
+//        Kruskal.mainTwo();
+//        HabrGraph.mainTree();
+
+
+
+    }
+}
